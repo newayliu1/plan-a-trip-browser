273f7afa0cfd15686c9b6038662eacb202761fb2
Wei Liu <newayliu1@gmail.com>
2017-03-27 17:52:52 -0400

    Create Trip model
    - Generate trip model
    - Generate trip route
    - Generate trip template
    - Add relationship between user and trip.
    - Add handlebars to show all trip
    - data binding with all related layers.

--- app/models/trip.js
+++ app/models/trip.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  email: DS.attr('string'),
-  trips: DS.hasMany('trip'),
+  name: DS.attr('string'),
+  user: DS.belongsTo('user')
 });
