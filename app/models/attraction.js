8e2a1ba9416e6189b214f6ffa456b84e0c0b6cc8
Wei Liu <newayliu1@gmail.com>
2017-03-29 11:13:57 -0400

    Add Attraction Box Style
    - Add box style to wrap all attractions

--- app/models/attraction.js
+++ app/models/attraction.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  location: DS.attr('string'),
+  address: DS.attr('string'),
   timeSpend: DS.attr('number'),
   notes: DS.attr('string'),
   trip: DS.belongsTo('trip'),
