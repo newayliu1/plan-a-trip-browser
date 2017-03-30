9bc5d2223b40380a7c523f984ac752e154b2625b
Wei Liu <newayliu1@gmail.com>
2017-03-30 16:07:44 -0400

    Beautify Pages
    - Add div and class, box, to wrap all the information
    - Add button design on all the buttons
    - Add backward button on trips and trip pages
    - Add create new trip button on trips page

--- app/components/trip-item.js
+++ app/components/trip-item.js
@@ -8,5 +8,13 @@ export default Ember.Component.extend({
     edit() {
       this.sendAction('edit', this.get('trip'));
     },
-  }
+  },
+  afterSomeEvent: function() {
+  this.flashMessage({
+    type: 'warning',
+    action: 'delete', // Run when message is
+    content: '<strong>Click here</strong> to cancel', // Pass HTML
+    duration: 5000, // Milliseconds to show the message for
+  });
+},
 });
